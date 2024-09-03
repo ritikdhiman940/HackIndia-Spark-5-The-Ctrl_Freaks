00121dd1b41d2cb2e2c758bd914abff8649af5ca
RITIK DHIMAN <145935768+ritikdhiman940@users.noreply.github.com>
2024-09-03 08:24:09 +0530

    Update NFTTile.js

--- src/components/NFTTile.js
+++ src/components/NFTTile.js
@@ -1,30 +1,27 @@
-import axie from "../tile.jpeg";
-import {
-    BrowserRouter as Router,
-    Link,
-  } from "react-router-dom";
-  import { GetIpfsUrlFromPinata } from "../utils";
+import { Link } from "react-router-dom";
+import { GetIpfsUrlFromPinata } from "../utils";
 
-function NFTTile (data) {
-    const newTo = {
-        pathname:"/nftPage/"+data.data.tokenId
-    }
-
-    const IPFSUrl = GetIpfsUrlFromPinata(data.data.image);
+function NFTTile({ data }) {
+    const { tokenId, image, name, description } = data;
+    const newTo = `/nftPage/${tokenId}`;
+    const IPFSUrl = GetIpfsUrlFromPinata(image);
 
     return (
         <Link to={newTo}>
-        <div className="border-2 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-2xl">
-            <img src={IPFSUrl} alt="" className="w-72 h-80 rounded-lg object-cover" crossOrigin="anonymous" />
-            <div className= "text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
-                <strong className="text-xl">{data.data.name}</strong>
-                <p className="display-inline">
-                    {data.data.description}
-                </p>
+            <div className="border-2 border-gray-700 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-lg transition-transform transform hover:scale-105">
+                <img 
+                    src={IPFSUrl} 
+                    alt={name} 
+                    className="w-full h-60 md:h-72 rounded-t-lg object-cover"
+                    crossOrigin="anonymous" 
+                />
+                <div className="text-white w-full p-4 bg-gradient-to-t from-gray-800 to-transparent rounded-b-lg">
+                    <strong className="text-xl">{name}</strong>
+                    <p className="text-sm mt-2">{description}</p>
+                </div>
             </div>
-        </div>
         </Link>
-    )
+    );
 }
 
 export default NFTTile;

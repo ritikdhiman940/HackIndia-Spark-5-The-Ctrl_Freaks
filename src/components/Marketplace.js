c047bd52875d568978fba061130ae23b1a2af715
RITIK DHIMAN <145935768+ritikdhiman940@users.noreply.github.com>
2024-09-03 08:23:22 +0530

    Update Marketplace.js
    some changes

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -2,94 +2,90 @@ import Navbar from "./Navbar";
 import NFTTile from "./NFTTile";
 import MarketplaceJSON from "../Marketplace.json";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { GetIpfsUrlFromPinata } from "../utils";
+import { ethers } from "ethers";
 
 export default function Marketplace() {
-const sampleData = [
-    {
-        "name": "NFT#1",
-        "description": "First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmfR9RNeQXPSRLkfUFywKeQwonbCQBfX7YDBTktivXQpkR",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#2",
-        "description": "Second NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmfR9RNeQXPSRLkfUFywKeQwonbCQBfX7YDBTktivXQpkR",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#3",
-        "description": "Third NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmfR9RNeQXPSRLkfUFywKeQwonbCQBfX7YDBTktivXQpkR",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-];
-const [data, updateData] = useState(sampleData);
-const [dataFetched, updateFetched] = useState(false);
+    const sampleData = [
+        {
+            "name": "NFT#1",
+            "description": "First NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmfR9RNeQXPSRLkfUFywKeQwonbCQBfX7YDBTktivXQpkR",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+        },
+        {
+            "name": "NFT#2",
+            "description": "Second NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmfR9RNeQXPSRLkfUFywKeQwonbCQBfX7YDBTktivXQpkR",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        },
+        {
+            "name": "NFT#3",
+            "description": "Third NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmfR9RNeQXPSRLkfUFywKeQwonbCQBfX7YDBTktivXQpkR",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        },
+    ];
 
-async function getAllNFTs() {
-    const ethers = require("ethers");
-    //After adding your Hardhat network to your metamask, this code will get providers and signers
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    //Pull the deployed contract instance
-    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
-    //create an NFT Token
-    let transaction = await contract.getAllNFTs()
+    const [data, updateData] = useState(sampleData);
+    const [dataFetched, updateFetched] = useState(false);
 
-    //Fetch all the details of every NFT from the contract and display
-    const items = await Promise.all(transaction.map(async i => {
-        var tokenURI = await contract.tokenURI(i.tokenId);
-        console.log("getting this tokenUri", tokenURI);
-        tokenURI = GetIpfsUrlFromPinata(tokenURI);
-        let meta = await axios.get(tokenURI);
-        meta = meta.data;
+    useEffect(() => {
+        async function fetchNFTs() {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer);
+            let transaction = await contract.getAllNFTs();
 
-        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-        let item = {
-            price,
-            tokenId: i.tokenId.toNumber(),
-            seller: i.seller,
-            owner: i.owner,
-            image: meta.image,
-            name: meta.name,
-            description: meta.description,
+            const items = await Promise.all(transaction.map(async i => {
+                var tokenURI = await contract.tokenURI(i.tokenId);
+                tokenURI = GetIpfsUrlFromPinata(tokenURI);
+                let meta = await axios.get(tokenURI);
+                meta = meta.data;
+
+                let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+                let item = {
+                    price,
+                    tokenId: i.tokenId.toNumber(),
+                    seller: i.seller,
+                    owner: i.owner,
+                    image: meta.image,
+                    name: meta.name,
+                    description: meta.description,
+                }
+                return item;
+            }));
+
+            updateFetched(true);
+            updateData(items);
         }
-        return item;
-    }))
 
-    updateFetched(true);
-    updateData(items);
-}
+        if (!dataFetched) {
+            fetchNFTs();
+        }
+    }, [dataFetched]);
 
-if(!dataFetched)
-    getAllNFTs();
-
-return (
-    <div>
-        <Navbar></Navbar>
-        <div className="flex flex-col place-items-center mt-20">
-            <div className="md:text-xl font-bold text-white">
-                Top NFTs
+    return (
+        <div className="bg-gray-900 min-h-screen">
+            <Navbar />
+            <div className="flex flex-col items-center mt-20 p-4">
+                <h1 className="text-3xl font-bold text-white mb-8">Top NFTs</h1>
+                <div className="flex flex-wrap justify-center gap-6">
+                    {data.map((value, index) => (
+                        <NFTTile data={value} key={index} />
+                    ))}
+                </div>
             </div>
-            <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
-                {data.map((value, index) => {
-                    return <NFTTile data={value} key={index}></NFTTile>;
-                })}
-            </div>
-        </div>            
-    </div>
-);
-
+        </div>
+    );
 }
